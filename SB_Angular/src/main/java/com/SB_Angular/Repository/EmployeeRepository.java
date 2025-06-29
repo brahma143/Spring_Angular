5de21fab428ccbb49e6ccda194f83b2c63aec27c
Brahma530@ <brahmareddy.90lingam@outlook.com>
2025-06-29 20:12:55 +0530

    update the files

--- SB_Angular/src/main/java/com/SB_Angular/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SB_Angular.Repository;
-
-import com.SB_Angular.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
